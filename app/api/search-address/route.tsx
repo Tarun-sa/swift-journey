4242701c3ff5522018918b0907758897036e29a7
apple <apple@apples-MacBook-Pro.local>
2024-11-03 11:24:17 +0530

    feat/done mapbox setup

---
+++ app/api/search-address/route.tsx
@@ -0,0 +1,25 @@
+import { NextResponse } from "next/server";
+
+const BASE_URL='https://api.mapbox.com/search/searchbox/v1/suggest';
+
+// Function to generate a UUID
+function generateUUID() {
+    return crypto.randomUUID();
+}
+
+export async function GET(request:any) {
+
+    const {searchParams}= new URL(request.url);
+    const searchText=searchParams.get('q')
+    const sessionToken = generateUUID(); 
+
+    const response = await fetch(
+        `${BASE_URL}?q=${searchText}&language=en&limit=6&session_token=${sessionToken}&country=IN&access_token=${process.env.MAPBOX_ACCESS_TOKEN}`
+    );
+
+    const data=await response.json()
+
+  
+    return NextResponse.json({result:data})
+    
+}
--- app/page.tsx
+++ app/page.tsx
@@ -1,6 +1,4 @@
 import Booking from '@/components/Booking'
-import { UserButton } from '@clerk/nextjs'
-import Image from 'next/image'
 
 export default function Home() {
   return (
